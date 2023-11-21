7fd1113903f7aa9851abcaae11b288666b44d921
lzpzx666 <2112568058@qq.com>
2023-11-21 14:27:07 +0800

    项目管理相关代码

--- bidding-business/src/main/java/com/neteast/business/domain/template/TemplateDict.java
+++ bidding-business/src/main/java/com/neteast/business/domain/template/TemplateDict.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.neteast.common.core.domain.BaseEntity;
 import lombok.Data;
 
 /**
@@ -14,29 +15,42 @@ import lombok.Data;
 
 @Data
 @TableName("template_dict")
-public class TemplateDict {
+public class TemplateDict extends BaseEntity{
 
     /** 主键id */
     @TableId(value = "id",type = IdType.AUTO)
-    private Integer id ;
+    private Integer id;
 
     /** 主键名称 */
     @TableField("key")
-    private String key ;
+    private String key;
 
     /** 键值 */
     @TableField("value")
-    private String value ;
-
-    /** 项目类型 */
-    @TableField("project_type")
-    private String projectType ;
-
-    /** 招标类型 */
-    @TableField("bidding_type")
-    private String biddingType ;
+    private String value;
 
     /** 是否通用 */
     @TableField("common")
     private String common ;
+
+    /** 项目类型 */
+    @TableField("project_type")
+    private String projectType;
+
+    /** 招标类型 */
+    @TableField("bidding_type")
+    private String biddingType;
+
+    /** 文件类型 */
+    @TableField("file_type")
+    private String fileType;
+
+    /** 字典类型 单选 多选 填值等 */
+    @TableField("dict_type")
+    private String dictType;
+
+    /** 是否有效 */
+    @TableField("use")
+    private Integer use;
+
 }
