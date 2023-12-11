fcc546ae1310e565fc9b65cfa53d5ff16d7d824c
lzpzx666 <2112568058@qq.com>
2023-12-11 17:58:49 +0800

    评标阶段相关代码

--- bidding-admin/src/main/java/com/neteast/web/controller/ws/SocketIOListener.java
+++ bidding-admin/src/main/java/com/neteast/web/controller/ws/SocketIOListener.java
@@ -37,21 +37,23 @@ public class SocketIOListener implements DataListener<String> {
     public void onData(SocketIOClient client, String s, AckRequest ackRequest){
         log.info("客户端-{},发送消息-{}",client.getRemoteAddress(),s);
         OperaRecord operaRecord = JSONObject.parseObject(s,OperaRecord.class);
-        operaRecord.setTime(new Date());
-        String receiver = operaRecord.getReceiver();
-        String channel = operaRecord.getChannel();
-        //设置用户选择的信息
-        setExpertSelectRecord(operaRecord);
-        //获取接收方
-        List<Custom> customs = SocketIOMessageEventHandler.getSocketIOByRole(receiver,channel);
-        //记录操作
-        SocketIOMessageEventHandler.setOperaRecord(operaRecord.getChannel(),operaRecord);
-        customs.forEach(o->{
-            //实时操作状态
-            SocketIOClient socketIOClient = socketIOServer.getClient(o.getUuid());
-            String body = JSON.toJSONString(operaRecord);
-            SocketIOService.sendMsg(o,body,socketIOClient);
-        });
+        if (operaRecord!=null){
+            operaRecord.setTime(new Date());
+            String receiver = operaRecord.getReceiver();
+            String channel = operaRecord.getChannel();
+            //设置用户选择的信息
+            setExpertSelectRecord(operaRecord);
+            //获取接收方
+            List<Custom> customs = SocketIOMessageEventHandler.getSocketIOByRole(receiver,channel);
+            //记录操作
+            SocketIOMessageEventHandler.setOperaRecord(operaRecord.getChannel(),operaRecord);
+            customs.forEach(o->{
+                //实时操作状态
+                SocketIOClient socketIOClient = socketIOServer.getClient(o.getUuid());
+                String body = JSON.toJSONString(operaRecord);
+                SocketIOService.sendMsg(o,body,socketIOClient);
+            });
+        }
     }
 
     /**
@@ -78,6 +80,7 @@ public class SocketIOListener implements DataListener<String> {
         ExpertBidMsg expertBidMsg = new ExpertBidMsg();
         setExpertBidMsg(expertBidMsg,operaRecord);
         list.add(expertBidMsg);
+        map.put(channel,list);
     }
 
     private static ExpertBidMsg setExpertBidMsg(ExpertBidMsg expertBidMsg,OperaRecord operaRecord){
@@ -88,6 +91,7 @@ public class SocketIOListener implements DataListener<String> {
         expertBidMsg.setPackageId(operaRecord.getPackageId());
         String type = operaRecord.getOperaType();
         String opera = operaRecord.getRecord();
+        log.info("操作记录的值为-{}",opera);
         switch (type){
             case "commercial":
                 Commercial commercial = JSONObject.parseObject(opera, Commercial.class);
