f7897e9736868a90a2eeecc9c82868e97b690b10
lzpzx666 <2112568058@qq.com>
2023-12-19 13:40:04 +0800

    基础信息相关代码

--- bidding-admin/src/main/java/com/neteast/web/controller/custom/AgencyMessageController.java
+++ bidding-admin/src/main/java/com/neteast/web/controller/custom/AgencyMessageController.java
@@ -1,12 +1,18 @@
 package com.neteast.web.controller.custom;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.neteast.business.domain.custom.AgencyMessage;
 import com.neteast.business.service.IAgencyMessageService;
 import com.neteast.common.core.controller.BaseController;
+import com.neteast.common.core.domain.AjaxResult;
+import com.neteast.common.core.page.PageDomain;
+import com.neteast.common.core.page.TableDataInfo;
+import com.neteast.common.core.page.TableSupport;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author lzp
@@ -19,6 +25,45 @@ public class AgencyMessageController extends BaseController{
 
     @Resource
     IAgencyMessageService agencyMessageService;
+
+    @GetMapping("/listByPage")
+    public AjaxResult getAgencyMessageListByPage(AgencyMessage agencyMessage){
+
+        startPage();
+        PageDomain pageDomain = TableSupport.getPageDomain();
+        List<AgencyMessage> list = agencyMessageService.getAgencyMessageList(agencyMessage);
+        TableDataInfo info = getDataTable(list);
+        JSONObject body = initPageParams(info,pageDomain.getPageSize(),pageDomain.getPageNum());
+        return success(body);
+    }
+
+    @GetMapping("/list")
+    public AjaxResult getAgencyMessageList(AgencyMessage agencyMessage){
+
+        List<AgencyMessage> list = agencyMessageService.getAgencyMessageList(agencyMessage);
+        return success(list);
+    }
+
+    @PostMapping("/add")
+    public AjaxResult addAgencyMessage(@RequestBody AgencyMessage agencyMessage){
+
+        agencyMessageService.save(agencyMessage);
+        return success();
+    }
+
+    @PostMapping("/update")
+    public AjaxResult updateAgencyMessage(@RequestBody AgencyMessage agencyMessage){
+
+        agencyMessageService.updateById(agencyMessage);
+        return success();
+    }
+
+    @PostMapping("/del/{id}")
+    public AjaxResult delAgencyMessage(@PathVariable("id")Integer id){
+
+        agencyMessageService.removeById(id);
+        return success();
+    }
 
 
 
--- bidding-admin/src/main/java/com/neteast/web/controller/custom/PurchaserMessageController.java
+++ bidding-admin/src/main/java/com/neteast/web/controller/custom/PurchaserMessageController.java
@@ -1,11 +1,18 @@
 package com.neteast.web.controller.custom;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.neteast.business.domain.custom.PurchaserMessage;
 import com.neteast.business.service.IPurchaserMessageService;
 import com.neteast.common.core.controller.BaseController;
+import com.neteast.common.core.domain.AjaxResult;
+import com.neteast.common.core.page.PageDomain;
+import com.neteast.common.core.page.TableDataInfo;
+import com.neteast.common.core.page.TableSupport;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author lzp
@@ -19,6 +26,43 @@ public class PurchaserMessageController extends BaseController {
     @Resource
     IPurchaserMessageService purchaserMessageService;
 
+    @GetMapping("/listByPage")
+    public AjaxResult getPurchaserMessageListByPage(PurchaserMessage purchaserMessage){
 
+        startPage();
+        PageDomain pageDomain = TableSupport.getPageDomain();
+        List<PurchaserMessage> list = purchaserMessageService.getPurchaserMessageList(purchaserMessage);
+        TableDataInfo info = getDataTable(list);
+        JSONObject body = initPageParams(info,pageDomain.getPageSize(),pageDomain.getPageNum());
+        return success(body);
+    }
+
+    @GetMapping("/list")
+    public AjaxResult getPurchaserMessageList(PurchaserMessage purchaserMessage){
+
+        List<PurchaserMessage> list = purchaserMessageService.getPurchaserMessageList(purchaserMessage);
+        return success(list);
+    }
+
+    @PostMapping("/add")
+    public AjaxResult addPurchaserMessage(@RequestBody PurchaserMessage purchaserMessage){
+
+        purchaserMessageService.save(purchaserMessage);
+        return success();
+    }
+
+    @PostMapping("/update")
+    public AjaxResult updatePurchaserMessage(@RequestBody PurchaserMessage purchaserMessage){
+
+        purchaserMessageService.updateById(purchaserMessage);
+        return success();
+    }
+
+    @PostMapping("/del/{id}")
+    public AjaxResult delPurchaserMessage(@PathVariable("id") Integer id){
+
+        purchaserMessageService.removeById(id);
+        return success();
+    }
 
 }
